ff58e555ad45ccff592bfdde14e19fd7be9af987
muhammadyusuf1202 <muhammadyusuf27062012@gmail.com>
2026-02-23 13:52:42 +0300

    Create Message.js

---
+++ backend/models/Message.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  avatar: String,
+  text: {
+    type: String,
+    required: true,
+    maxlength: 2000
+  },
+  isEdited: {
+    type: Boolean,
+    default: false
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
